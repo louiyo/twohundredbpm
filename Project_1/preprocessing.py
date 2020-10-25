e741a04b5da005594f320216c8e59a677f9216df
Camil Hamdane <36880466+louiyo@users.noreply.github.com>
2020-10-25 12:22:05 +0100

    Update preprocessing.py

--- Project_1/preprocessing.py
+++ Project_1/preprocessing.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 
+def preprocess(data):
+
+    data = remove_irrelevant_columns(data)
+
+    for idx in range(len(data)):
+        data[idx] = replace_non_defined(data[idx])
+        data[idx] = data[idx][:, np.nanstd(data[idx], axis=0) != 0]
+        data[idx], _, _ = standardize(data[idx])
+
+    return data
+
+
 def standardize(x):
     """Standardize the original data set."""
 
@@ -20,10 +32,10 @@ def replace_non_defined(data):
     return np.where(np.isnan(data), np.nanmedian(data, axis=0), data)
 
 
-def remove_non_defined_columns(data):
+def remove_irrelevant_columns(data):
     """
         Remove the columns where all examples from a specific PRI_jet_num have
-        non defined values (nan).
+        non defined values (-999), thus being irrelevant to the model.
     """
 
     for jet_num in range(len(data)):
