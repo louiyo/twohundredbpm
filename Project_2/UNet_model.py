09951cf2f1c11781ed600c0c6a9482b980460fe5
Camil Hamdane <36880466+louiyo@users.noreply.github.com>
2020-12-06 21:31:36 +0100

    added metrics functions

--- Project_2/UNet_model.py
+++ Project_2/UNet_model.py
@@ -3,6 +3,26 @@ import numpy as np
 from keras import Model
 from keras.layers import BatchNormalization, MaxPooling2D, concatenate, Dropout, Conv2D, Conv2DTranspose, Activation
 from keras.optimizers import Adam
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 
 def conv_batch(img, n_filters, batch_norm, activation_fct, kernel_size):
