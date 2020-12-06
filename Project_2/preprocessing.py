a608fa96b29f9c9a8befbaca97e8c21f5894fe52
Camil Hamdane <36880466+louiyo@users.noreply.github.com>
2020-12-06 21:40:31 +0100

    Update preprocessing.py

--- Project_2/preprocessing.py
+++ Project_2/preprocessing.py
@@ -1,38 +1,60 @@
 from data_augmentation import *
 from sklearn.model_selection import train_test_split
 
+TEST_IMG_SIZE = 608
+TRAIN_IMG_SIZE = 400
 
-def load_image(directory):
-    return mpimg.imread(directory)
+def pad(img):
+    size_diff = TEST_IMG_SIZE - img.shape[0]
+    return np.pad(img, pad_width = ((0, size_diff), (0, size_diff), (0,0)), mode='reflect')
 
 
-def preprocess(root_dir='./training/', save_dir='',
-               patches=False, divide_set=False, ratio=0.25):
+def preprocess(root_dir='./training/',
+               divide_set=True,
+               ratio=0.1,
+               upscale_to_test_size=True,
+               save_imgs = False):
     images_dir = root_dir + 'images/'
     gt_dir = root_dir + 'groundtruth/'
     files = os.listdir(images_dir)
 
     images = [PIL.Image.open(images_dir + img) for img in files]
     gt_images = [PIL.Image.open(gt_dir + img) for img in files]
-    rdaug = RandAugment(12, 12)
+    rdaug = RandAugment(5, 10, upscale_to_test_size)
 
     imgs_aug = []
     gt_imgs_aug = []
 
-    for (img, gt_img, file_, i) in zip(images, gt_images, files, range(len(images))):
-        print("processing image ", i, "/", len(images))
-        for j in range(3):
+    for (img_, gt_img_, file_, i) in zip(images, gt_images, files, range(len(images))):
+        print("processing image ", i+1, "/", len(images), ':(')
+        imgs_aug.append(pad(img_to_array(img_)))
+        gt_imgs_aug.append(pad(img_to_array(gt_img_)))
+        for j in range(2):
+            img, gt_img = img_.copy(), gt_img_.copy()
             img, gt_img = rdaug.augment((img, gt_img))
-            imgs_aug.append(img_to_array(img))
-            gt_imgs_aug.append(img_to_array(gt_img))
-            #img.save(images_dir + str(i) + file_)
-            #gt_img.save(gt_dir + '_' + str(i) + file_)
+            img, gt_img = img_to_array(img), img_to_array(gt_img)
+            
+            
+            if(upscale_to_test_size):
+                size_diff = TEST_IMG_SIZE - img.shape[0]
+                img = pad(img) #np.pad(img, pad_width = ((0, size_diff), (0, size_diff), (0,0)), mode='reflect')
+                gt_img = pad (gt_img) #np.pad(gt_img, pad_width = ((0, size_diff), (0, size_diff), (0,0)), mode='reflect')
+            
+            imgs_aug.append(img)
+            gt_imgs_aug.append(gt_img)
+            if(save_imgs):
+                array_to_img(img).save(images_dir + 'augmented' + str(j) + file_)
+                array_to_img(gt_img).save(gt_dir + 'augmented' + str(j) + file_)
 
-    imgs_aug = np.asarray([img_to_array(img_) for img_ in imgs_aug])
-    gt_imgs_aug = np.asarray([img_to_array(img_) for img_ in gt_imgs_aug])
     if(divide_set == False):
-        return imgs_aug, gt_imgs_aug
+        imgs_aug = np.stack(imgs_aug, axis = 0)
+        gt_imgs_aug = np.stack(gt_imgs_aug, axis = 0)
+        return (imgs_aug, gt_imgs_aug)
     else:
         X_train, X_test, Y_train, Y_test = train_test_split(imgs_aug, gt_imgs_aug,
-                                                            test_size=ratio, random_state=0)
+                                                            test_size=ratio, random_state=2020)
+        X_train = np.stack(X_train, axis = 0) / 255.0
+        X_test = np.stack(X_test, axis = 0)/ 255.0
+        Y_train = np.stack(Y_train, axis = 0)/ 255.0
+        Y_test = np.stack(Y_test, axis = 0)/ 255.0
         return X_train, X_test, Y_train, Y_test
