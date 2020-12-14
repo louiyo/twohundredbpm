5eb06582732e047da6b5723308caa3014525197c
NTN <44361217+GBNTN@users.noreply.github.com>
2020-12-14 10:50:52 +0100

    Update run for fractal net
    + adding loading of X_test and Y_test (split of train data) for evaluation

--- Project_2/run.py
+++ Project_2/run.py
@@ -6,6 +6,7 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCh
 from tensorflow.keras.optimizers import Adam
 from proj2_helpers import create_submission, load_test_imgs, make_predictions
 from preprocessing import *
+from fractal_net import *
 
 IMG_HEIGHT = IMG_WIDTH = 400
 IMG_CHANNELS = 3
@@ -22,11 +23,14 @@ SUBMISSION_PATH = './submission/new_submission.csv'
 BATCH_SIZE = 8
 DILATION = True
 
+NUM_FILTERS = 16
+DEPTH = 3
+ALPHA = 0.1
+DROPOUT = 0.5
 
 
 
-
-def run_(train = False, use_VGG = False, save_imgs = False, upscale=False):
+def run_(train = False, use_VGG = False, use_fractal = False, save_imgs = False, upscale=False):
     #If upscaling is used, training images are padded with a mirror reflection.
     if(upscale): img_size=608
     else: img_size=400
@@ -36,34 +40,45 @@ def run_(train = False, use_VGG = False, save_imgs = False, upscale=False):
     else:
         if(not train):
             input_size = Input((img_size, img_size, IMG_CHANNELS))
-            model = build_unet(input_size,
-                                n_filters=N_FILTERS,
-                                dropout_down=DROPOUT_DOWN,
-                                dropout_up=DROPOUT_UP,
-                                batch_norm=True,
-                                activation_fct=ACTIV_FCT,
-                                final_activation=FINAL_ACT,
-                                kernel_size=KERNEL_SIZE,
-                                dilate=DILATION)
+            if use_fractal:
+                model = build_fract_model(input_size,
+                                          filters = NUM_FILTERS,
+                                          dropout = DROPOUT,
+                                          depth=DEPTH,
+                                          alpha = ALPHA,
+                                          kernel_size = KERNEL_SIZE)
+            else: 
+                model = build_unet(input_size,
+                                    n_filters=N_FILTERS,
+                                    dropout_down=DROPOUT_DOWN,
+                                    dropout_up=DROPOUT_UP,
+                                    batch_norm=True,
+                                    activation_fct=ACTIV_FCT,
+                                    final_activation=FINAL_ACT,
+                                    kernel_size=KERNEL_SIZE,
+                                    dilate=DILATION)
             model.load_weights('./checkpoints/bestmodel.h5')
             print('loaded weigths from ', 'bestmodel.h5')
         else:
             print('beginning training')
-            X_train, Y_train = preprocess(divide_set=False,
+            X_train, X_test, Y_train, Y_test = preprocess(divide_set=True,
                                          save_imgs = save_imgs,
                                          upscale_to_test_size=upscale)
+            
+            if use_fractal:
+                Y_train = labels_to_hot_matrix(Y_train)
 
-            model = train_model(X_train, Y_train, img_size)
+            model = train_model(X_train, Y_train, X_test, Y_test, img_size)
 
         imgs_test = load_test_imgs(TEST_IMGS_PATH)
 
         print("making predictions...")
-        make_predictions(imgs_test, model, img_size)
+        make_predictions(imgs_test, model, img_size, use_fractal = use_fractal)
         print("created submission")
 
 
 
-def train_model(X_train, Y_train, img_size):
+def train_model(X_train, Y_train, X_test, Y_test, img_size):
 
     cp = ModelCheckpoint(filepath=MODEL_FILEPATH,
                         verbose=1,
@@ -84,32 +99,40 @@ def train_model(X_train, Y_train, img_size):
     model_tools = [cp,es,lr]
 
     input_size = Input((img_size, img_size, IMG_CHANNELS))
+    
+    if use_fractal:
+        model = build_fract_model(input_size,
+                                  filters = N_FILTERS,
+                                  dropout = DROPOUT,
+                                  depth=DEPTH,
+                                  alpha = ALPHA,
+                                  kernel_size = KERNEL_SIZE)
+    else:
+        model = build_unet(input_size,
+                           n_filters=N_FILTERS,
+                           dropout_down=DROPOUT_DOWN,
+                           dropout_up=DROPOUT_UP,
+                           batch_norm=True,
+                           activation_fct=ACTIV_FCT,
+                           final_activation=FINAL_ACT,
+                           kernel_size=KERNEL_SIZE,
+                           dilate = DILATION)
 
-    unet_model = build_unet(input_size,
-                            n_filters=N_FILTERS,
-                            dropout_down=DROPOUT_DOWN,
-                            dropout_up=DROPOUT_UP,
-                            batch_norm=True,
-                            activation_fct=ACTIV_FCT,
-                            final_activation=FINAL_ACT,
-                            kernel_size=KERNEL_SIZE,
-                            dilate = DILATION)
+    model.compile(optimizer=Adam(lr=1e-4),
+                  loss='binary_crossentropy',
+                  metrics=['binary_accuracy',
+                            f1_m,
+                            precision_m,
+                            recall_m])
 
-    unet_model.compile(optimizer=Adam(lr=1e-4),
-                       loss='binary_crossentropy',
-                       metrics=['binary_accuracy',
-                                f1_m,
-                                precision_m,
-                                recall_m])
-
-    unet_model.fit(X_train, Y_train,
-                   validation_split=0.15,
-                   batch_size=BATCH_SIZE,
-                   epochs=EPOCHS,
-                   callbacks=model_tools)
+    model.fit(X_train, Y_train,
+              validation_split=0.15,
+              batch_size=BATCH_SIZE,
+              epochs=EPOCHS,
+              callbacks=model_tools)
 
     # fitting the model to the train data
     # evaluating performance of the model
     print("evaluating performance of the model")
-    print(unet_model.evaluate(X_test, Y_test))
-    return unet_model
+    print(model.evaluate(X_test, Y_test))
+    return model
